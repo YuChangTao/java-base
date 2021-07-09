e556a18b123028da524985d325b398de1cb7468f
bme_yutao <2686222074@qq.com>
2021-07-09 16:45:09 +0800

    ThreadLocal测试

---
+++ java8Test/src/main/java/com/yt/juc/ThreadLocalTest.java
@@ -0,0 +1,46 @@
+package com.yt.juc;
+
+/**
+ * ThreadLocal 的作用：提供线程内的局部变量，这种变量在线程的生命周期内起作用，减少同一个线程内多个函数或
+ * 者组件之间一些公共变量的传递的复杂度。
+ */
+public class ThreadLocalTest {
+
+    static ThreadLocal<String> localVar = new ThreadLocal<>();
+
+    static void print(String str) {
+        //打印当前线程中本地内存中本地变量的值
+        System.out.println(str + " :" + localVar.get());
+        //清除本地内存中的本地变量
+        localVar.remove();
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //设置线程1中本地变量的值
+                localVar.set("localVar1");
+                //调用打印方法
+                print("thread1");
+                //打印本地变量
+                System.out.println("after remove : " + localVar.get());
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //设置线程1中本地变量的值
+                localVar.set("localVar2");
+                //调用打印方法
+                print("thread2");
+                //打印本地变量
+                System.out.println("after remove : " + localVar.get());
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
